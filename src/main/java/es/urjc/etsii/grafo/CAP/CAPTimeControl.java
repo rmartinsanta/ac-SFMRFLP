bdf96332d063e9859bb99fac453655fa8ab99764
Raúl Martín <raul.martin@urjc.es>
2024-01-09 12:24:20 +0100

    Add more reference values extracted from sota SFMRFLP, set a timelimit of 5 seconds if no reference value is found for any given instance

--- src/main/java/es/urjc/etsii/grafo/CAP/CAPTimeControl.java
+++ src/main/java/es/urjc/etsii/grafo/CAP/CAPTimeControl.java
@@ -10,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class CAPTimeControl extends TimeLimitCalculator<CAPSolution, CAPInstance> {
 
+    public static final long DEFAULT_IF_UNKNOWN = 5_000; // 5 seconds if UNKNOWN REF VALUE
     private final CAPReferenceResults referenceResults;
 
     public CAPTimeControl(CAPReferenceResults referenceResults) {
@@ -18,7 +19,14 @@ public class CAPTimeControl extends TimeLimitCalculator<CAPSolution, CAPInstance
 
     @Override
     public long timeLimitInMillis(CAPInstance instance, Algorithm<CAPSolution, CAPInstance> algorithm) {
-        var nanos = this.referenceResults.getValueFor(instance.getId()).getTimeInNanos();
+        var refResult = referenceResults.getValueFor(instance.getId());
+        if(refResult == null){
+            return DEFAULT_IF_UNKNOWN;
+        }
+        var nanos = refResult.getTimeInNanos();
+        if(nanos <= 0){
+            return DEFAULT_IF_UNKNOWN; // Zero or negative values are invalid time limits
+        }
         var timelimit = TimeUtil.convert(nanos, TimeUnit.NANOSECONDS, TimeUnit.MILLISECONDS) * 2;
         return Math.max(1_000, timelimit); // If the timelimit is really small, use 1 second as timelimit.
     }
