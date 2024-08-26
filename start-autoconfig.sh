b4a97469d7d22f754fbcfbe47155d8fc08d3bab2
Raúl Martín <raul.martin@urjc.es>
2024-08-26 16:30:01 +0100

    Upgrade ot irace 3.9

--- start-autoconfig.sh
+++ start-autoconfig.sh
@@ -4,7 +4,7 @@ mvn clean package
 java \
 -Xmx24G \
 -Xms24G \
--jar target/SFMRFLP-0.19-SNAPSHOT.jar \
+-jar target/SFMRFLP-0.21-SNAPSHOT.jar \
 --autoconfig \
 --solver.parallelExecutor=true \
 --solver.nWorkers=32
